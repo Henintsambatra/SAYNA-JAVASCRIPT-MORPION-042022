1bae4f4a698be93cba78b6935ce42b2b0458f0e6
HENINTSAMBATRA\monpc <najoromevasjaspers@gmail.com>
2022-04-29 16:49:56 +0300

    javascript
    le meilleur modification

---
+++ Assets/Morpion.js
@@ -0,0 +1,14 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATION = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+]
+const cellElements = document.querySelectorAll('[data - cell} ')
